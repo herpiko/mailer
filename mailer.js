f609c8db9b2fef9eb0a42c859e76ed7298b60e75
Pranav Lakshminarayanan <plakshm1@jhu.edu>
2016-01-14 16:02:58 -0500

    Shells for other emails

--- mailer.js
+++ mailer.js
@@ -10,7 +10,7 @@ var domain = 'sandbox23741fa183b1467da1bd5d449dc1645e.mailgun.org';
 var address = 'postmaster@' + domain;
 
 /**
-  Send a welcom email
+  Send a welcome email
   @param name - name of the user.
   @param toAddress - recipient's email address
 **/
@@ -28,7 +28,91 @@ exports.welcome = function(name, toAddress) {
         'subject': 'Welcome to FusiformCAST!',
         'text': 'Hello, ' + name + '. Welcome to FusiformCAST!'
       }
-  }, function(error, response, body){
+  }, function(error, response, body) {
+      if(error) {
+          console.log(error);
+      } else {
+          console.log(body);
+      }
+  });
+}
+
+/**
+  Send an email to reset the password
+  @param name - name of the user.
+  @param toAddress - recipient's email address
+**/
+exports.passReset = function(name, toAddress) {
+  request({
+      url: 'https://api.mailgun.net/v3/' + domain + '/messages',
+      method: 'POST',
+      'auth': {
+        'user': 'api',
+        'password': api_key
+      },
+      form: {
+        'from': address,
+        'to': toAddress,
+        'subject': 'FusiformCAST Password Reset',
+        'text': 'Hello, ' + name + '. Click here to reset your password.'
+      }
+  }, function(error, response, body) {
+      if(error) {
+          console.log(error);
+      } else {
+          console.log(body);
+      }
+  });
+}
+
+/**
+  Send a notification that the password was changed
+  @param name - name of the user.
+  @param toAddress - recipient's email address
+**/
+exports.passChanged = function(name, toAddress) {
+  request({
+      url: 'https://api.mailgun.net/v3/' + domain + '/messages',
+      method: 'POST',
+      'auth': {
+        'user': 'api',
+        'password': api_key
+      },
+      form: {
+        'from': address,
+        'to': toAddress,
+        'subject': 'FusiformCAST Password Change Notification',
+        'text': 'Hello, ' + name + '. Your FusiformCAST password was recently changed.'
+      }
+  }, function(error, response, body) {
+      if(error) {
+          console.log(error);
+      } else {
+          console.log(body);
+      }
+  });
+}
+
+/**
+  Send a notification that the password was changed
+  @param name - name of the user.
+  @param toAddress - recipient's email address
+**/
+exports.tfaReset = function(name, toAddress) {
+  request({
+      url: 'https://api.mailgun.net/v3/' + domain + '/messages',
+      method: 'POST',
+      'auth': {
+        'user': 'api',
+        'password': api_key
+      },
+      form: {
+        'from': address,
+        'to': toAddress,
+        'subject': 'FusiformCAST Two-Factor Authentication Reset',
+        'text': 'Hello, ' + name + '. Click here to reset your two-factor authentication.'
+      }
+  }, function(error, response, body) {
       if(error) {
           console.log(error);
       } else {
--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 var mailer = require('./mailer.js');
 
 mailer.welcome('Pranav', 'pranz.laks@gmail.com');
+mailer.passReset('Pranav', 'pranz.laks@gmail.com');
+mailer.passChanged('Pranav', 'pranz.laks@gmail.com');
+mailer.tfaReset('Pranav', 'pranz.laks@gmail.com');
