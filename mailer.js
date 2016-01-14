e0cd0b8fe04791a7cc63f724878ae0eeb9e704c8
Pranav Lakshminarayanan <plakshm1@jhu.edu>
2016-01-14 15:25:37 -0500

    Mailer module working
    with 'Welcome!' email

--- mailer.js
+++ mailer.js
@@ -1,23 +1,38 @@
-var requests = require('requests');
-var bodyParser = require('body-parser');
-
-//Express
-var express = require('express');
-var app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+var request = require('request');
 
 //API KEY
 var api_key = 'key-80ebd8a4062b3ca35560541b9b714a38';
 
-//Domain
+// Domain
 //  NOTE: Sandbox domain for testing only
-var domain = 'postmaster@sandbox23741fa183b1467da1bd5d449dc1645e.mailgun.org';
+//        Change this later
+var domain = 'sandbox23741fa183b1467da1bd5d449dc1645e.mailgun.org';
+var address = 'postmaster@' + domain;
 
-
-
-//Create the server
-var server = app.listen(8081, 'localhost', function() {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('Listening on ' + host + ':' + port + "\n");
-});
+/**
+  Send a welcom email
+  @param name - name of the user.
+  @param toAddress - recipient's email address
+**/
+exports.welcome = function(name, toAddress) {
+  request({
+      url: 'https://api.mailgun.net/v3/' + domain + '/messages',
+      method: 'POST',
+      'auth': {
+        'user': 'api',
+        'password': api_key
+      },
+      form: {
+        'from': address,
+        'to': toAddress,
+        'subject': 'Welcome to FusiformCAST!',
+        'text': 'Hello, ' + name + '. Welcome to FusiformCAST!'
+      }
+  }, function(error, response, body){
+      if(error) {
+          console.log(error);
+      } else {
+          console.log(body);
+      }
+  });
+}
--- test.js
+++ test.js
@@ -1,37 +1,3 @@
-var request = require('request');
-var bodyParser = require('body-parser');
+var mailer = require('./mailer.js');
 
-// API KEY
-var api_key = 'key-80ebd8a4062b3ca35560541b9b714a38';
-
-// Domain
-//  NOTE: Sandbox domain for testing only
-var domain = 'sandbox23741fa183b1467da1bd5d449dc1645e.mailgun.org';
-var address = 'postmaster@' + domain;
-
-var toAddress = 'pranz.laks@gmail.com';
-
-// LOG
-console.log('\n' + domain);
-console.log(address + '\n');
-
-request({
-    url: 'https://api.mailgun.net/v3/' + domain + '/messages',
-    method: 'POST',
-    'auth': {
-      'user': 'api',
-      'password': api_key
-    },
-    form: {
-      'from': address,
-      'to': toAddress,
-      'subject': 'Mailgun test from Node',
-      'text': 'This message was sent from Node. Yayyy!'
-    }
-}, function(error, response, body){
-    if(error) {
-        console.log(error);
-    } else {
-        console.log(body);
-    }
-});
+mailer.welcome('Pranav', 'pranz.laks@gmail.com');
